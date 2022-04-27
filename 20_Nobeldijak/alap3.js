415dd8a63b8ad7fa92b1fb14400ca00338386db2
lordkorom <91594449+lordkorom@users.noreply.github.com>
2022-04-27 09:09:59 +0200

    sad

---
+++ 20_Nobeldijak/alap3.js
@@ -0,0 +1,40 @@
+const d=new Date();
+let year =d.getFullYear();
+var sz='';
+for (let i =year; i > 1900; i--) {
+    sz+='<option value="'+i+'">'+i+'</option>'
+    
+}
+document.getElementById("lenyilo").innerHTML=sz;
+function evkiiras()
+{
+    var kivalasztottev=document.getElementById("lenyilo").value;
+    console.log(kivalasztottev);
+    fetch("https://api.nobelprize.org/v1/prize.json")
+.then(x => x.json())
+.then(y => {
+    console.log(y);
+    var sz="";
+    sz+='<ul>'
+    for(elem of y.prizes)
+{
+    
+    if(elem.year==kivalasztottev)
+    {sz+='<li>'
+        sz+=elem.year;
+       
+        sz+='<button data-bs-toggle="collapse" data-bs-target="#demo'+elem.category+'">Collapsible</button>'
+
+        sz+='<div id="'+elem.category+'" class="collapse">'
+        for(szemely of elem.laureates)
+        sz+=szemely.firstname+" "+szemely.surname+"<br>"
+        sz+='</div> '
+        sz+='</li>'
+    }
+    
+    
+}
+sz+='</ul>'
+document.getElementById("kiiras").innerHTML=sz;
+});
+}
